68e63e06039fd409316c5393d99a6c073d312006
george sp <spanosg@unisystems.gr>
2016-10-18 09:30:39 +0300

    add files for LAMP stack

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-#
-#
+yum update -y --exclude=kernel
 
-yum install -y git
+yum install git unzip screen
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
 
-#
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+service httpd restart
+
+
+
 
