1dfc9be383146c6a96f5a4cba2071d67cc94a1ff
george sp <spanosg@unisystems.gr>
2016-10-18 10:27:30 +0300

    some adjust

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-yum update -y --exclude=kernel
+#yum update -y --exclude=kernel
 
-yum install git unzip screen
-yum install -y httpd httpd-devel httpd-tools
-chkconfig --add httpd
-chkconfig httpd on
-service httpd stop
+#yum install git unzip screen
+#yum install -y httpd httpd-devel httpd-tools
+#chkconfig --add httpd
+#chkconfig httpd on
+#service httpd stop
 
-rm -rf /var/www/html
-ln -s /vagrant /var/www/html
+#rm -rf /var/www/html
+#ln -s /vagrant /var/www/html
 
-service httpd start
+#service httpd start
 
 # PHP
-yum install -y php php-cli php-common php-devel php-mysql
+#yum install -y php php-cli php-common php-devel php-mysql
 
 # MySQL
-yum install -y mysql mysql-server mysql-devel
-chkconfig mysqld on
+#yum install -y mysql mysql-server mysql-devel
+#chkconfig mysqld on
 
-service mysqld start
+#service mysqld start
 
-mysql -u root -e "SHOW DATABASES";
+#mysql -u root -e "SHOW DATABASES";
 
 service httpd restart
 
 
-
+cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/spanosg/lamp/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/spanosg/lamp/master/files/info.php
+
+
+service httpd restart
